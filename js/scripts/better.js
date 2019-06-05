aa3570594a247dee6efcf2ab4cb6f55a7161b5ae
Olga Duka <olgaduk@mail.ru>
2019-06-05 21:54:44 +0600

    Добавляет блок достопримечательностей

---
+++ js/scripts/better.js
@@ -0,0 +1,13 @@
+// Функция переключения разделов
+//function toggleSectionBetterPlace() {
+//  var selector = $(this).val();
+
+//  $('.version.current').find('.version__toggle').each(function (i, el) {
+//    $('.version__toggle').toggleClass('version__toggle--active');
+//  });
+
+// Получаем заголовок и "обещание" для выбранной целевой аудитории
+//  var title = $(this).data().title;
+//  var promise = $(this).data().promise;
+//  viewComplectation(selector, title, promise);
+//};
--- js/scripts/slider.js
+++ js/scripts/slider.js
@@ -56,3 +56,102 @@ $('.stop__slider').slick({
     }
   ]
 });
+
+$('.details__slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  initialSlide:3,
+  centerMode: true,
+  arrows: false,
+  asNavFor: '.details__slider-nav',
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: TABLET_WIDTH,
+      slidesToShow: 3
+    }
+  ]
+});
+
+$('.details__slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  initialSlide:3,
+  asNavFor: '.details__slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: TABLET_WIDTH,
+      slidesToShow: 6
+    }
+  ]
+});
+
+// Параметры слайдеров достопримечательностей
+const setSlickParameters = function(arrowPrev, arrowNext) {
+  const slickParameters = {
+    mobileFirst: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    //arrows: !0,
+    prevArrow: $(arrowPrev),
+    nextArrow: $(arrowNext),
+    responsive: [
+      { breakpoint: TABLET_WIDTH,
+        settings: {
+          centerMode: false
+        }
+      },
+      { breakpoint: DESKTOP_WIDTH,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  }
+  return slickParameters;
+};
+
+const better1 = setSlickParameters('.better__arrow--prev1', '.better__arrow--next1');
+const better2 = setSlickParameters('.better__arrow--prev2', '.better__arrow--next2');
+const better3 = setSlickParameters('.better__arrow--prev3', '.better__arrow--next3');
+const better4 = setSlickParameters('.better__arrow--prev4', '.better__arrow--next4');
+
+$('.better__slider--1').slick(better1);
+$('.better__slider--2').slick(better2);
+$('.better__slider--3').slick(better3);
+$('.better__slider--4').slick(better4);
+
+const menu = document.querySelector('.better__menu');
+const betterMenuItems = menu.querySelectorAll('.better__menu-item');
+const betterSliders = document.querySelectorAll('.better__slider');
+
+$('.better__menu-item').click(function (evt) {
+  if ($(this).hasClass('better__menu-item--active')) {
+    return;
+  } else {
+    const selector = $(this).data().num;
+    const targetSlider = $('.better__slider[data-num="' + selector + '"]'); // Селект нужного блока
+    const targetControl = $('.better__controls[data-num="' + selector + '"]');
+
+    $('.better__slider').each(function (i, el) {
+      $(el).removeClass('better__slider--active');
+    });
+    $('.better__controls').each(function (i, el) {
+      $(el).removeClass('better__controls--active');
+    });
+    $('.better__menu-item').each(function (i, el) {
+      $(el).removeClass('better__menu-item--active');
+    });
+
+    $(this).addClass('better__menu-item--active');
+    targetSlider.addClass('better__slider--active');
+    targetControl.addClass('better__controls--active');
+    targetSlider.slick('refresh');
+  }
+});
